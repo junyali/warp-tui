1208c76176d654101b8220f9a2358f9c5209a237
junya <junjia.liu@pm.me>
2025-12-30 19:19:05 +0000

    feat: add functionality to dump, stats, and rotate-keys

--- src/warp_tui/screens/screen_settings/tunnel.py
+++ src/warp_tui/screens/screen_settings/tunnel.py
@@ -1,8 +1,178 @@
 from textual.screen import ModalScreen
-from textual.widgets import OptionList, Footer, Static
-from textual.containers import Container
+from textual.widgets import OptionList, Footer, Static, Button
+from textual.containers import Container, Vertical, VerticalScroll
 from textual.binding import Binding
 from textual.app import ComposeResult
+
+from ...utils import WarpCLI
+
+class DumpTunnel(ModalScreen):
+    CSS = """
+    DumpTunnel {
+        align: center middle;
+    }
+
+    #info-dialogue {
+        width: 80%;
+        height: auto;
+        border: solid orange;
+        padding: 1;
+        margin: 2 8;
+    }
+
+    #info-title {
+        text-align: center;
+        text-style: bold;
+        color: orange;
+        margin-bottom: 1;
+    }
+
+    #info-content {
+        height: auto;
+        padding: 1;
+        margin-bottom: 1;
+    }
+    
+    #info-scroll {
+        height: auto;
+        max-height: 60vh;
+    }
+
+    #info-button {
+        width: 100%;
+        margin-top: 1;
+    }
+    """
+
+    BINDINGS = [
+        Binding("escape", "dismiss", "Close"),
+        Binding("enter", "dismiss", "Close")
+    ]
+
+    def compose(self) -> ComposeResult:
+        dump, success = WarpCLI.dump_tunnel()
+
+        with Vertical(id="info-dialogue"):
+            yield Static("Split Tunnel Routing Dump", id="info-title")
+            with VerticalScroll(id="info-scroll"):
+                if success:
+                    yield Static(dump, id="info-content")
+                else:
+                    yield Static(f"{dump}", id="info-content")
+            yield Button("OK", variant="primary", id="info-button")
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        self.dismiss()
+
+class StatsTunnel(ModalScreen):
+    CSS = """
+    StatsTunnel {
+        align: center middle;
+    }
+
+    #info-dialogue {
+        width: 80%;
+        height: auto;
+        border: solid orange;
+        padding: 1;
+        margin: 2 8;
+    }
+
+    #info-title {
+        text-align: center;
+        text-style: bold;
+        color: orange;
+        margin-bottom: 1;
+    }
+
+    #info-content {
+        height: auto;
+        padding: 1;
+        margin-bottom: 1;
+    }
+
+    #info-scroll {
+        height: auto;
+        max-height: 60vh;
+    }
+
+    #info-button {
+        width: 100%;
+        margin-top: 1;
+    }
+    """
+
+    BINDINGS = [
+        Binding("escape", "dismiss", "Close"),
+        Binding("enter", "dismiss", "Close")
+    ]
+
+    def compose(self) -> ComposeResult:
+        stats, success = WarpCLI.get_stats()
+
+        with Vertical(id="info-dialogue"):
+            yield Static("Current Tunnel Connection Stats", id="info-title")
+            with VerticalScroll(id="info-scroll"):
+                if success:
+                    yield Static(stats, id="info-content")
+                else:
+                    yield Static(f"{stats}", id="info-content")
+            yield Button("OK", variant="primary", id="info-button")
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        self.dismiss()
+
+class RotateKeysTunnel(ModalScreen):
+    CSS = """
+    RotateKeysTunnel {
+        align: center middle;
+    }
+
+    #info-dialogue {
+        width: 60%;
+        height: auto;
+        border: solid orange;
+        padding: 1;
+        margin: 2 8;
+    }
+
+    #info-title {
+        text-align: center;
+        text-style: bold;
+        color: orange;
+        margin-bottom: 1;
+    }
+
+    #info-content {
+        height: auto;
+        padding: 1;
+        margin-bottom: 1;
+    }
+
+    #info-button {
+        width: 100%;
+        margin-top: 1;
+    }
+    """
+
+    BINDINGS = [
+        Binding("escape", "dismiss", "Close"),
+        Binding("enter", "dismiss", "Close")
+    ]
+
+    def compose(self) -> ComposeResult:
+        success, error_msg = WarpCLI.rotate_keys()
+
+        with Vertical(id="info-dialogue"):
+            yield Static("Generate New Key-Pair", id="info-title")
+            if success:
+                yield Static("Success!", id="info-content")
+            else:
+                yield Static(error_msg, id="info-content")
+            yield Button("OK", variant="primary", id="info-button")
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        self.dismiss()
 
 class TunnelSettings(ModalScreen):
     CSS = """
@@ -57,3 +227,9 @@ class TunnelSettings(ModalScreen):
         if option == "Back":
             self.app.pop_screen()
             return
+        elif option == "Dump":
+            self.app.push_screen(DumpTunnel())
+        elif option == "Stats":
+            self.app.push_screen(StatsTunnel())
+        elif option == "Rotate Keys":
+            self.app.push_screen(RotateKeysTunnel())
